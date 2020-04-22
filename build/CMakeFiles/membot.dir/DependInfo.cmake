fb0df12ebe24b84711c83bea2ce9e7bb7ecd7201
Ivan Cheng <ivan@ivancheng.co.uk>
2020-04-22 17:59:17 +0100

    Initial setup

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/src/chatbot.cpp" "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/src/chatgui.cpp" "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/src/chatlogic.cpp" "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/src/graphedge.cpp" "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/src/graphnode.cpp" "/Users/IvanCheng/Documents/GitHub/Memory-Management-Chatbot-in-Cpp/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_FILE_OFFSET_BITS=64"
+  "__WXMAC__"
+  "__WXOSX_COCOA__"
+  "__WXOSX__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/osx_cocoa-unicode-static-3.1"
+  "/usr/local/include/wx-3.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
